deba740bd8cdc89bb0abc4e200201f8d031141e2
Shreyas Raj Singh <srs231198@gmail.com>
2019-03-17 02:27:09 -0500

    createGrid fuction operational

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -6,7 +7,7 @@ struct node{
 
     char symbol;
 
-    //
+    //connecting nodes to create the structure
     node *right;
     node *left;
     node *up;
@@ -14,19 +15,99 @@ struct node{
 
 };
 
-void createList(node *&);
-void printList(node *&);
+//function to create the grid on which the user will draw
+void createGrid(node *&);
+//function to take in input and process comands respectively
+void Input(fstream &);
+//function to print the grid!
+//void printGrid(node *&);
+//function to delete the grid
+//void deleteGrid(node *&);
 
 int main(){
 
-    node *head = nullptr;
+    //create a file input object
+    fstream inputFile;
 
-    createList(head);
+    Input(inputFile);
+
+    //create the head pointer and assign it to nullptr
+    node *head = new node;
+
+
+    createGrid(head);
+
+//    printGrid(head);
 
     return 0;
 
 }
 
-void createList(node *&){
+void createGrid(node *&head){
+
+    //Assign a temp pointer to the head node
+    node *temp = head;
+
+    //Create the first row of 50 nodes
+    //loop 49 times since the first node is already there
+    for(int i = 0; i < 50; i++){
+        //create a new node
+        node *newNode = new node;
+        newNode->right = nullptr;
+        newNode->left = nullptr;
+        //link the temp node to newNode
+        temp->right = newNode;
+        newNode->left = temp;
+        //chenge temp's position
+        temp = temp->right;
+    }
+
+    //create a temporary pointer to hold head's location
+    node *temp1 = head;
+
+    //for the next 49 nodes
+    for(int i = 1; i < 50; i++){
+        node *newNode1 = new node;
+        node *temp2 = newNode1;
+        //and the columns on each row
+            for(int i = 0; i < 50 ; i++){
+                //create new node
+                node *newNode2 = new node;
+                //link the nodes to the appropriate places
+                temp2->up = temp1;
+                temp1->down = temp2;
+                temp2->right = newNode2;
+                newNode2->left = temp2;
+                //assign the pointers to new ositions
+                temp1 = temp1->right;
+                temp2 = temp2->right;
+            }
+        //assign temp1 to a new position
+        temp1 = newNode1;
+    }
 
 }
+
+void Input(fstream &inputFile){
+    //open the command file
+    inputFile.open("commands.txt");
+
+    //check for the proper opening of the file
+    if(!inputFile.good()){
+        exit(0);
+    }
+
+    //string object to store the line commmand
+    string text, command;
+
+    //string objects to store the individual commands
+    string penStatus, direction, distancs, boldOrNot;
+
+    while(inputFile >> text){
+
+
+
+    }
+}
+
+
